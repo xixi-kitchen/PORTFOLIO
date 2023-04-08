8017d05fce2ef71f482d0c0d1475bb6a9971c7ac
xixi-kitchen <1850786422@qq.com>
2023-04-08 15:23:15 +0800

    一次暂存内容

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -4,19 +4,28 @@ import Tcc from "./Tcc";
 // import Scene from "./Splinefibe";
 // import Excali from "./Excali";
 import "tailwindcss/tailwind.css";
+import Blog from "./blog/Blog";
+
+
+
 
 function App() {
   return (
     <div className="w-screen static">
-      <div className="top-0 container w-1/2 lg:w-3/4 md,sm:w-full ">
+      <div className="top-0 mx-auto w-600 ">
         <Ttt />
         <Tcc />
         <h1 className="text-3xl font-bold underline text-yellow-200 bg-yellow-900">Hello world!</h1>
         <h1 className="text-3xl font-bold underline bg-green-400" >Hello world!</h1>
-      </div>
+        <Blog />
+        
+      </div> 
       
     </div>
   );
 }
 
 export default App;
+
+
+

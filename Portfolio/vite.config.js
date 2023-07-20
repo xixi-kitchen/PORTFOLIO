3bc816c46032c1493be2df21afa0c9c77a8bdccf
xixikitchen <57753951+xixi-kitchen@users.noreply.github.com>
2023-07-20 15:42:48 +0800

    7.20正式上线

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -5,34 +5,32 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 5000, // 加大限制的大小将500kb改成1500kb或者更大
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            // 分解块，将大块分解成更小的块,在vite.config.js当中的build下面进行配置
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-            // 但是生成的文件都在dist下面的assets文件下，里面既有js、css等等。
-          }
-        },
-        // 可以将不同的文件放在不同的文件下
-        chunkFileNames: (chunkInfo) => {
-          const facadeModuleId = chunkInfo.facadeModuleId
-            ? chunkInfo.facadeModuleId.split("/")
-            : [];
-          const fileName =
-            facadeModuleId[facadeModuleId.length - 2] || "[name]";
-          return `js/${fileName}/[name].[hash].js`;
-        },
-      },
-    },
+    chunkSizeWarningLimit: 6000, // 加大限制的大小将500kb改成1500kb或者更大
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes("node_modules")) {
+    //         // 分解块，将大块分解成更小的块,在vite.config.js当中的build下面进行配置
+    //         return id
+    //           .toString()
+    //           .split("node_modules/")[1]
+    //           .split("/")[0]
+    //           .toString();
+    //         // 但是生成的文件都在dist下面的assets文件下，里面既有js、css等等。
+    //       }
+    //     },
+    //     // 可以将不同的文件放在不同的文件下
+    //     chunkFileNames: (chunkInfo) => {
+    //       const facadeModuleId = chunkInfo.facadeModuleId
+    //         ? chunkInfo.facadeModuleId.split("/")
+    //         : [];
+    //       const fileName =
+    //         facadeModuleId[facadeModuleId.length - 2] || "[name]";
+    //       return `js/${fileName}/[name].[hash].js`;
+    //     },
+    //   },
+    // },
   },
 
-  publicPath: "./",
-  outputDir: "dist",
-  assetsDir: "static",
+
 });
